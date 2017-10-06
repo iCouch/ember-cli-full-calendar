7c2f619021d89d7fa828a43a7316747d8eca9a20
igor <igorstajic273@gmail.com>
2017-10-06 16:22:45 +0200

    FIxes fastboot.

--- blueprints/full-calendar/index.js
+++ blueprints/full-calendar/index.js
@@ -2,9 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    var _this = this;
-    return this.addBowerPackageToProject('fullcalendar').then(function() {
-      return _this.addAddonToProject('ember-cli-moment-shim', '0.6.2');
-    });
+    this.addAddonToProject('ember-cli-moment-shim');
   }
 };
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 /* jshint node: true */
 'use strict';
 
+var MergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
+var path = require('path');
+var map = require('broccoli-stew').map;
+
 module.exports = {
   name: 'ember-cli-full-calendar',
 
-  included: function(app) {
+  included: function (app) {
     this._super.included(app);
+    app.import('vendor/fullcalendar/fullcalendar.min.js');
+    app.import('vendor/fullcalendar/fullcalendar.min.css');
+    
+  },
+  treeForVendor(defaultTree) {
+    var calendarJS = new Funnel( path.dirname(require.resolve('fullcalendar')), {
+      destDir: 'fullcalendar',
+      files:['fullcalendar.min.js']});
+    var calendarCSS = new Funnel( path.dirname(require.resolve('fullcalendar')), {
+        destDir: 'fullcalendar',
+        files:['fullcalendar.min.css']});
+        
+    calendarJS = map(calendarJS, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+    
 
-    app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.js');
-    app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.css');
+    return new MergeTrees([defaultTree, calendarJS, calendarCSS]);
   }
 };
